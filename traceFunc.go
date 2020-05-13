dfc9837eeaa73fe90229f4106511b9c2358e7af0
MaYacong <mayacong@hotmail.com>
2020-05-13 16:24:52 +0800

    记录函数的进入、退出、持续时间

---
+++ traceFunc.go
@@ -0,0 +1,27 @@
+package main
+
+/*
+跟踪函数的进入、退出、持续时间。
+*/
+
+import (
+    "log"
+    "time"
+)
+
+func main() {
+    sample()
+}
+
+func sample() {
+    defer trace("func_sample")()    // 别漏了()
+    time.Sleep(time.Second * 3)
+}
+
+func trace(funcName string) func() {
+    start := time.Now()
+    log.Printf("enter %s", funcName)
+    return func() {
+        log.Printf("exit %s (duration %s)", funcName, time.Since(start))
+    }
+}
