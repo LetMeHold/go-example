24e014524c7fe281d3224c82c84b739b1be1c6ca
MaYacong <mayacong@hotmail.com>
2020-05-27 11:22:38 +0800

    处理系统信号

---
+++ osSignal.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func main() {
+	t := time.NewTicker(time.Second)
+	c := make(chan os.Signal)
+	signal.Notify(c)
+	for {
+		select {
+		case <-t.C:
+			log.Println("程序运行中...")
+		case s := <-c:
+			switch s {
+			case syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT, syscall.SIGHUP:
+				log.Println("接收到退出信号: ", s)
+				os.Exit(0)
+			default:
+				log.Println("接收到其他信号: ", s)
+			}
+		}
+	}
+}
