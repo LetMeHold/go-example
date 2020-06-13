a501d847c29a044ed06ad1e40861f86050332edf
letmehold <mayacong@hotmail.com>
2020-06-13 18:17:06 +0800

    增加带密码key的用法

--- remoteCommond.go
+++ remoteCommond.go
@@ -9,15 +9,17 @@ import (
 
 func main() {
 	//client, err := NewConnByPwd("172.18.180.110:22", "root", "password")
-	client, err := NewConnByKey("172.18.180.110:22", "root")
+	//client, err := NewConnByKey("172.18.180.110:22", "root")
+	client, err := NewConnByKeyWithPwd("172.18.180.110:22", "root", "/root/.ssh/id_rsa_pwd", "key_password")
 	if err != nil {
-		log.Fatalf("NewConnByPwd failed : %v", err)
+		log.Fatalf("Create new connect failed : %v", err)
 	}
 	if err := Commond(client, "locale"); err != nil {
 		log.Fatalf("Commond failed : %v", err)
 	}
 }
 
+// NewConnByPwd 通过用户名密码建立连接
 func NewConnByPwd(host, usr, pwd string) (*ssh.Client, error) {
 	config := &ssh.ClientConfig{
 		User: usr,
@@ -38,6 +40,7 @@ func NewConnByPwd(host, usr, pwd string) (*ssh.Client, error) {
 	return client, nil
 }
 
+// NewConnByKey 通过免密key建立连接
 func NewConnByKey(host, usr string) (*ssh.Client, error) {
 	var keyFile string
 	if usr == "root" {
@@ -67,6 +70,31 @@ func NewConnByKey(host, usr string) (*ssh.Client, error) {
 	return client, nil
 }
 
+// NewConnByKeyWithPwd 通过带密码的key建立连接
+func NewConnByKeyWithPwd(host, usr, keyFile, keyPwd string) (*ssh.Client, error) {
+	key, err := ioutil.ReadFile(keyFile)
+	if err != nil {
+		return nil, err
+	}
+	signer, err := ssh.ParsePrivateKeyWithPassphrase(key, []byte(keyPwd))
+	if err != nil {
+		return nil, err
+	}
+	config := &ssh.ClientConfig{
+		User: usr,
+		Auth: []ssh.AuthMethod{
+			ssh.PublicKeys(signer),
+		},
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(), // 接受所有hostkey
+	}
+	client, err := ssh.Dial("tcp", host, config)
+	if err != nil {
+		return nil, err
+	}
+	return client, nil
+}
+
+// Commond 在远程主机执行一条命令
 func Commond(client *ssh.Client, cmd string) error {
 	session, err := client.NewSession()
 	if err != nil {
